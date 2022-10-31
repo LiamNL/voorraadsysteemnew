fcc6e0cf306ecc00e91620bd2f33364087d09fb6
LiamNL <91883556+LiamNL@users.noreply.github.com>
2022-10-31 13:50:53 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace voorraadsysteemnew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnexit = new System.Windows.Forms.Button();
+            this.dgvgerecht = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvgerecht)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(901, 458);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(143, 26);
+            this.btnexit.TabIndex = 1;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // dgvgerecht
+            // 
+            this.dgvgerecht.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvgerecht.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvgerecht.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvgerecht.Location = new System.Drawing.Point(12, 12);
+            this.dgvgerecht.Name = "dgvgerecht";
+            this.dgvgerecht.RowHeadersWidth = 51;
+            this.dgvgerecht.RowTemplate.Height = 24;
+            this.dgvgerecht.Size = new System.Drawing.Size(231, 347);
+            this.dgvgerecht.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(1056, 496);
+            this.Controls.Add(this.dgvgerecht);
+            this.Controls.Add(this.btnexit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvgerecht)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.DataGridView dgvgerecht;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace voorraadsysteemnew
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            NpgsqlConnection conn = new NpgsqlConnection("server=10.10.10.40;port=5432;Database=tappas;User Id=softwareguys;Password=T^jTc4rv*!M6FKYVybFbuQG&v^HVkJ$KfRd1PCuAc$$@rEVvrJvKH%9#7gFQ;");
+            conn.Open();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn;
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "select * from voorraadsysteem";
+            NpgsqlDataReader dr = comm.ExecuteReader();
+            if (dr.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dgvgerecht.DataSource = dt;
+            }
+            comm.Dispose();
+            conn.Close();
+
+
+
+
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit the application", "Exit message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
