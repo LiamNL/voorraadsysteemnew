f9bf741181b4a6d4ec921d87c5ca40bbb6fcc03b
LiamNL <91883556+LiamNL@users.noreply.github.com>
2022-11-25 11:41:08 +0100

    added buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.dgvgerecht = new System.Windows.Forms.DataGridView();
             this.txtnaam = new System.Windows.Forms.TextBox();
             this.txtgewicht = new System.Windows.Forms.TextBox();
+            this.lblnaam = new System.Windows.Forms.Label();
+            this.lblgewicht = new System.Windows.Forms.Label();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnrefresh = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvgerecht)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,7 +70,7 @@
             // 
             // txtnaam
             // 
-            this.txtnaam.Location = new System.Drawing.Point(697, 12);
+            this.txtnaam.Location = new System.Drawing.Point(646, 160);
             this.txtnaam.Name = "txtnaam";
             this.txtnaam.Size = new System.Drawing.Size(155, 22);
             this.txtnaam.TabIndex = 3;
@@ -72,17 +78,85 @@
             // 
             // txtgewicht
             // 
-            this.txtgewicht.Location = new System.Drawing.Point(697, 53);
+            this.txtgewicht.Location = new System.Drawing.Point(646, 221);
             this.txtgewicht.Name = "txtgewicht";
             this.txtgewicht.Size = new System.Drawing.Size(155, 22);
             this.txtgewicht.TabIndex = 4;
+            // 
+            // lblnaam
+            // 
+            this.lblnaam.BackColor = System.Drawing.Color.LightGreen;
+            this.lblnaam.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblnaam.Location = new System.Drawing.Point(646, 137);
+            this.lblnaam.Name = "lblnaam";
+            this.lblnaam.Size = new System.Drawing.Size(155, 20);
+            this.lblnaam.TabIndex = 5;
+            this.lblnaam.Text = "Naam";
+            this.lblnaam.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblnaam.Click += new System.EventHandler(this.lblnaam_Click);
+            // 
+            // lblgewicht
+            // 
+            this.lblgewicht.BackColor = System.Drawing.Color.LightGreen;
+            this.lblgewicht.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblgewicht.Location = new System.Drawing.Point(646, 196);
+            this.lblgewicht.Name = "lblgewicht";
+            this.lblgewicht.Size = new System.Drawing.Size(155, 22);
+            this.lblgewicht.TabIndex = 6;
+            this.lblgewicht.Text = "Gewicht";
+            this.lblgewicht.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(920, 61);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(124, 23);
+            this.btnupdate.TabIndex = 7;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.BackColor = System.Drawing.Color.LightGreen;
+            this.lblId.Location = new System.Drawing.Point(646, 64);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(155, 23);
+            this.lblId.TabIndex = 8;
+            this.lblId.Text = "ID";
+            this.lblId.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblId.Visible = false;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(646, 90);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(155, 22);
+            this.txtId.TabIndex = 9;
+            this.txtId.Visible = false;
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Location = new System.Drawing.Point(920, 107);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(124, 23);
+            this.btnrefresh.TabIndex = 10;
+            this.btnrefresh.Text = "Refresh";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGreen;
-            this.ClientSize = new System.Drawing.Size(1056, 496);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1106, 496);
+            this.Controls.Add(this.btnrefresh);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.lblgewicht);
+            this.Controls.Add(this.lblnaam);
             this.Controls.Add(this.txtgewicht);
             this.Controls.Add(this.txtnaam);
             this.Controls.Add(this.dgvgerecht);
@@ -101,6 +175,12 @@
         private System.Windows.Forms.DataGridView dgvgerecht;
         private System.Windows.Forms.TextBox txtnaam;
         private System.Windows.Forms.TextBox txtgewicht;
+        private System.Windows.Forms.Label lblnaam;
+        private System.Windows.Forms.Label lblgewicht;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnrefresh;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,21 +9,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Npgsql;
+using System.Data.SqlClient;
 
 namespace voorraadsysteemnew
 {
     public partial class Form1 : Form
     {
+        NpgsqlCommand command = new NpgsqlCommand();
+        string cmd;
+        NpgsqlConnection con = new NpgsqlConnection("server=10.10.10.40;port=5432;Database=tappas;User Id=softwareguys;Password=T^jTc4rv*!M6FKYVybFbuQG&v^HVkJ$KfRd1PCuAc$$@rEVvrJvKH%9#7gFQ;");
         public Form1()
         {
             InitializeComponent();
-            NpgsqlConnection conn = new NpgsqlConnection("server=10.10.10.40;port=5432;Database=tappas;User Id=softwareguys;Password=T^jTc4rv*!M6FKYVybFbuQG&v^HVkJ$KfRd1PCuAc$$@rEVvrJvKH%9#7gFQ;");
+            
             //hiermee maak je de connection
-            conn.Open();
+            con.Open();
             //hiermee zorg je dat de database open connectie staat voor de applicatie
             NpgsqlCommand comm = new NpgsqlCommand();
             //dit is dat alle commands hierin new worden gemaakt, voor als je al meer commands hebt van dit soort, bijvoorbeeld je hebt een command dat hij data moet vasthouden,
-            comm.Connection = conn;
+            comm.Connection = con;
            // dit zorgd dat de commands connecten met de database, zodat je ze kan gebruiken
             comm.CommandType = CommandType.Text;
             //dit is dat de command die je wilt doen als datatype een text type is.
@@ -40,10 +44,47 @@ namespace voorraadsysteemnew
             }
             comm.Dispose();
             //dit zorgt ervoor dat hij geen info onthoud dus hij laat alle info los.
-            conn.Close();
+            con.Close();
             //zorg ervoor dat de database gesloten word.
 
 
+        }
+
+
+        public void RefreshGridView()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                    RefreshGridView();
+                }
+                else if (con.State != ConnectionState.Open)
+                {
+                    con.Open();
+                    cmd = "select * from gerecht";
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        dgvgerecht.DataSource = dt;
+                        con.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No Records Found!!");
+                    con.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There are no Users Found!!");
+
+            }
         }
 
         private void btnexit_Click(object sender, EventArgs e)
@@ -63,6 +104,87 @@ namespace voorraadsysteemnew
         private void txtnaam_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void lblnaam_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            string _pId = txtId.Text;
+            string _naam = txtnaam.Text;
+            string _Gewicht = txtgewicht.Text;
+
+
+
+
+
+
+            if (txtId.Visible == false && lblId.Visible == false)
+            {
+                txtId.Visible = true;
+                lblId.Visible = true;
+            }
+
+            else if (!string.IsNullOrEmpty(_pId))
+            {
+                try
+                {
+                    int _Fid = Convert.ToInt32(txtId.Text);
+                    con.Open();
+
+                    cmd = "UPDATE gerecht SET Naam = @Naam, Gewicht = @Gewicht" + "WHERE Product_ID = @Product_ID";
+                    command = new NpgsqlCommand(cmd, con);
+
+                    command.Parameters.Add(new NpgsqlParameter("@Product_ID", _pId));
+                    command.Parameters.Add(new NpgsqlParameter("@Naam", _naam));
+                    command.Parameters.Add(new NpgsqlParameter("@Gewicht", _Gewicht));
+                    
+
+                   int _result1 = command.ExecuteNonQuery();
+
+                    if (_result1 > 0)
+                    {
+                        MessageBox.Show("User Updated");
+
+                        txtnaam.Text = "";
+                        txtId.Text = "";
+                        txtgewicht.Text = "";
+
+                        RefreshGridView();
+                        con.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("User not updated");
+                        con.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Record is not Updated or Found!" + ex);
+                    con.Close();
+                }
+                finally
+                {
+                    command.Dispose();
+                    con.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is an error");
+            }
+
+
+
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            RefreshGridView();
         }
     }
 }
